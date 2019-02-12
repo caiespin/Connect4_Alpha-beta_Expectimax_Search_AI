7ff5245a4e5d943263f4f13165f70b9e54c6155c
Sharonrab <srabinov@ucsc.edu>
2019-02-11 18:28:59 -0800

    first alphabeta algorithm

--- Player.py
+++ Player.py
@@ -26,7 +26,11 @@ class AIPlayer:
         RETURNS:
         The 0 based index of the column that represents the next move
         """
-        raise NotImplementedError('Whoops I don\'t know what to do')
+        valid_cols = []
+        for col in range(board.shape[1]):
+            if 0 in board[:,col]:
+                valid_cols.append(col)
+        pass
 
     def get_expectimax_move(self, board):
         """
@@ -49,12 +53,81 @@ class AIPlayer:
         RETURNS:
         The 0 based index of the column that represents the next move
         """
-        raise NotImplementedError('Whoops I don\'t know what to do')
+        col, minimax_score = minimax(board, 5, -math.inf, math.inf, True)
+        return col
+
+    def drop_piece(self, board, row, col, piece):
+        board[row][col] = piece
+
+    def is_valid_location(self, board, col):
+        return board[5-1][col] == 0
+
+    def get_next_open_row(self, board, col):
+        for r in range(5):
+            if board[r][col] == 0:
+                return r
+
+    def winning_move(self, board, piece):
+        player_win_str = '{0}{0}{0}{0}'.format(player_num)
+        to_str = lambda a: ''.join(a.astype(str))
+
+        def check_horizontal(b):
+            for row in b:
+                if player_win_str in to_str(row):
+                    return True
+            return False
+
+        def check_verticle(b):
+            return check_horizontal(b.T)
+
+        def check_diagonal(b):
+            for op in [None, np.fliplr]:
+                op_board = op(b) if op else b
+                
+                root_diag = np.diagonal(op_board, offset=0).astype(np.int)
+                if player_win_str in to_str(root_diag):
+                    return True
+
+                for i in range(1, b.shape[1]-3):
+                    for offset in [i, -i]:
+                        diag = np.diagonal(op_board, offset=offset)
+                        diag = to_str(diag.astype(np.int))
+                        if player_win_str in diag:
+                            return True
+
+            return False
+
+        return (check_horizontal(board) or
+                check_verticle(board) or
+                check_diagonal(board))        
+
+    def is_terminal_node(self, board):
+        valid_cols = []
+        for col in range(board.shape[1]):
+            if 0 in board[:,col]:
+                valid_cols.append(col)
+        return winning_move(board, 1) or winning_move(board, 2) or len(valid_cols) == 0
+
+    def evaluate_window(self, window, piece):
+        score = 0
+        opp_piece = 1
+        if piece == 1:
+            opp_piece = 2
+
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(0) == 1:
+            score += 5
+        elif window.count(piece) == 2 and window.count(0) == 2:
+            score += 2
+
+        if window.count(opp_piece) == 3 and window.count(0) == 1:
+            score -= 4
+
+        return score
 
 
-
-
-    def evaluation_function(self, board):
+    def evaluation_function(self, board, piece):
         """
         Given the current stat of the board, return the scalar value that 
         represents the evaluation function for the current player
@@ -72,9 +145,84 @@ class AIPlayer:
         RETURNS:
         The utility value for the current board
         """
-       
-       
-        return 0
+       score = 0
+
+        ## Score center column
+        center_array = [int(i) for i in list(board[:, 3])]
+        center_count = center_array.count(piece)
+        score += center_count * 3
+
+        ## Score Horizontal
+        for r in range(ROW_COUNT):
+            row_array = [int(i) for i in list(board[r,:])]
+            for c in range(COLUMN_COUNT-3):
+                window = row_array[c:c+4]
+                score += evaluate_window(window, piece)
+
+        ## Score Vertical
+        for c in range(COLUMN_COUNT):
+            col_array = [int(i) for i in list(board[:,c])]
+            for r in range(ROW_COUNT-3):
+                window = col_array[r:r+4]
+                score += evaluate_window(window, piece)
+
+        ## Score posiive sloped diagonal
+        for r in range(ROW_COUNT-3):
+            for c in range(COLUMN_COUNT-3):
+                window = [board[r+i][c+i] for i in range(4)]
+                score += evaluate_window(window, piece)
+
+        for r in range(ROW_COUNT-3):
+            for c in range(COLUMN_COUNT-3):
+                window = [board[r+3-i][c+i] for i in range(4)]
+                score += evaluate_window(window, piece)
+
+        return score
+
+    def minimax(board, depth, alpha, beta, maximizingPlayer):
+        valid_locations = get_valid_locations(board)
+        is_terminal = is_terminal_node(board)
+        if depth == 0 or is_terminal:
+            if is_terminal:
+                if winning_move(board, AI_PIECE):
+                    return (None, 100000000000000)
+                elif winning_move(board, PLAYER_PIECE):
+                    return (None, -10000000000000)
+                else: # Game is over, no more valid moves
+                    return (None, 0)
+            else: # Depth is zero
+                return (None, score_position(board, AI_PIECE))
+        if maximizingPlayer:
+            value = -math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = get_next_open_row(board, col)
+                b_copy = board.copy()
+                drop_piece(b_copy, row, col, AI_PIECE)
+                new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+                if new_score > value:
+                    value = new_score
+                    column = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+        else: # Minimizing player
+            value = math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = get_next_open_row(board, col)
+                b_copy = board.copy()
+                drop_piece(b_copy, row, col, PLAYER_PIECE)
+                new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+                if new_score < value:
+                    value = new_score
+                    column = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return column, value
 
 
 class RandomPlayer:
